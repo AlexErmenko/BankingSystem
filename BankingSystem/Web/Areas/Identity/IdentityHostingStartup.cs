618db2301e35ca4addee4a21b8fc9a7445792dd2
srJ001 <saltovka2001@gmail.com>
2020-03-03 17:50:43 +0200

    1

---
+++ BankingSystem/Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Web.Areas.Identity;
+
+[assembly: HostingStartup(typeof(IdentityHostingStartup))]
+
+namespace Web.Areas.Identity
+{
+	public class IdentityHostingStartup : IHostingStartup
+	{
+		public void Configure(IWebHostBuilder builder) { builder.ConfigureServices((context, services) => { }); }
+	}
+}
