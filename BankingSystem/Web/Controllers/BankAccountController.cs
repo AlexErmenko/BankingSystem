47ae1c1f8e1ba47048396dc823107e699a337f88
ThreePhaseCurrent <drakula00776@gmail.com>
2020-03-03 23:52:55 +0200

    Добавлен контроллер счетов, сигнатуры методов для закрытия\удаления счетов.

---
+++ BankingSystem/Web/Controllers/BankAccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Entity;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class BankAccountController : Controller
+	{
+		private IAsyncRepository<BankAccount> _repository;
+
+		public BankAccountController(IAsyncRepository<BankAccount> repositoryAccount)
+		{
+			_repository = repositoryAccount;
+		}
+
+		public IActionResult BankAccountClose(int idAccount)
+		{
+			return View("Index");
+		}
+
+		public IActionResult BankAccountDelete(int idAccount)
+		{
+			return View("Index");
+		}
+	}
+}
