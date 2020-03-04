bb687cab7709ce62092bb47a198e8056d7d0b62a
Alex <Alex.Eremenko.Memento@gmail.com>
2020-03-04 22:39:37 +0200

    2

--- BankingSystem/Web/Controllers/AdminController.cs
+++ BankingSystem/Web/Controllers/AdminController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ApplicationCore.Interfaces;
+﻿using ApplicationCore.Interfaces;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -10,96 +6,73 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
-    public class AdminController : Controller
+	public class AdminController : Controller
 	{
+		private IAsyncRepository<IdentityRole>    _roleRepository;
 		private IAsyncRepository<ApplicationUser> _userRepository;
-		private IAsyncRepository<IdentityRole> _roleRepository;
 
-		public AdminController(IAsyncRepository<ApplicationUser> userRepository, IAsyncRepository<IdentityRole> roleRepository)
+		public AdminController(IAsyncRepository<ApplicationUser> userRepository,
+							   IAsyncRepository<IdentityRole>    roleRepository)
 		{
 			_userRepository = userRepository;
 			_roleRepository = roleRepository;
 		}
 
 		// GET: Admin
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public ActionResult Index() { return View(); }
 
-        // GET: Admin/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		// GET: Admin/Details/5
+		public ActionResult Details(int id) { return View(); }
 
-        // GET: Admin/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: Admin/Create
+		public ActionResult Create() { return View(); }
 
-        // POST: Admin/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+		// POST: Admin/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				// TODO: Add insert logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+				return RedirectToAction(nameof(Index));
+			}
+			catch { return View(); }
+		}
 
-        // GET: Admin/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+		// GET: Admin/Edit/5
+		public ActionResult Edit(int id) { return View(); }
 
-        // POST: Admin/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+		// POST: Admin/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+				return RedirectToAction(nameof(Index));
+			}
+			catch { return View(); }
+		}
 
-        // GET: Admin/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+		// GET: Admin/Delete/5
+		public ActionResult Delete(int id) { return View(); }
 
-        // POST: Admin/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+		// POST: Admin/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+				return RedirectToAction(nameof(Index));
+			}
+			catch { return View(); }
+		}
+	}
 }
