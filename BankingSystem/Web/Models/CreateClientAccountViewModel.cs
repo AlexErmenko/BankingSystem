9e44f2182a1c05392122be6d3b057b20092a61b9
ThreePhaseCurrent <drakula00776@gmail.com>
2020-03-04 09:19:35 +0200

    Реализовано добавление, закрытие и удаление счетов клиента.

---
+++ BankingSystem/Web/Models/CreateClientAccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Entity;
+
+namespace Web.Models
+{
+	public class CreateClientAccountViewModel
+	{
+		public PhysicalPerson PhysicalPerson { get; set; }
+		public LegalPerson    LegalPerson    { get; set; }
+		public BankAccount    Account        { get; set; }
+		public string         ReturnUrl      { get; set; } = "/";
+	}
+}
