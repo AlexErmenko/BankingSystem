70d1e0b2e26a79467c8eb741b6f8efab5ac7d40f
srJ001 <saltovka2001@gmail.com>
2020-03-06 15:40:15 +0200

    1

--- BankingSystem/Web/ViewModels/UserViewModel.cs
+++ BankingSystem/Web/ViewModels/UserViewModel.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Infrastructure.Identity;
 
 namespace Web.ViewModels
 {
 	public class UserViewModel
 	{
-		public List<ApplicationUser> Users { get; set; }
-
-
+		public List<ApplicationUser> AppUsers { get; set; }
+		
 	}
 }
