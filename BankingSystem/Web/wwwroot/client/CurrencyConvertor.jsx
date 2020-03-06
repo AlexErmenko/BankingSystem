900dab714d9a2437f2f6b5349f7a290f798da199
Alex <Alex.Eremenko.Memento@gmail.com>
2020-03-06 16:05:17 +0200

    1

---
+++ BankingSystem/Web/wwwroot/client/CurrencyConvertor.jsx
@@ -0,0 +1,25 @@
+﻿import * as React from "../lib/react";
+
+class CommentBox extends React.Component {
+
+
+    constructor() {
+        super();
+        console.log("From comment box");
+
+    }
+
+    render() {
+        return(
+            <div className="commentBox">
+                React render
+
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <CommentBox/>,
+    document.getElementById("content"),
+);
