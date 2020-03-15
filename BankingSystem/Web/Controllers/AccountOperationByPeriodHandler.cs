cdd6241d85ff296a8d7df24a094e82f4dafc1f66
Alex <Alex.Eremenko.Memento@gmail.com>
2020-03-15 17:55:46 +0200

    1

---
+++ BankingSystem/Web/Controllers/AccountOperationByPeriodHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+
+using Web.Commands;
+
+namespace Web.Controllers
+{
+	public class AccountOperationByPeriodHandler: IRequestHandler<GetAccountOperationByPeriod,List<AccountOperationViewModel>>
+	{
+		public Task<List<AccountOperationViewModel>> Handle(GetAccountOperationByPeriod request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
