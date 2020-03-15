6c0959f88859ccb902e55b1a94dee1bf21cd0f3a
Alex <Alex.Eremenko.Memento@gmail.com>
2020-03-15 10:16:48 +0200

    1

---
+++ BankingSystem/Web/ViewModels/EditBankAccountViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Services
+{
+	public class EditBankAccountViewModel
+	{
+		public SelectList           SelectCurrencyList { get; set; }
+		public CurrencyConvertModel ConvertModel       { get; set; }
+	}
+}
