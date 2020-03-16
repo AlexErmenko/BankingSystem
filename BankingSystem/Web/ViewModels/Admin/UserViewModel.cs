31897f48de561699e1f17e8f94d7c68aac6ad51f
srJ001 <saltovka2001@gmail.com>
2020-03-16 20:52:11 +0200

    1

--- BankingSystem/Web/ViewModels/Admin/UserViewModel.cs
+++ BankingSystem/Web/ViewModels/Admin/UserViewModel.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
+using ApplicationCore.Entity;
 using Infrastructure.Identity;
 
 namespace Web.ViewModels.Admin
 {
 	public class UserViewModel
 	{
-		public object User { get; set; }
-		public List<ApplicationUser> AppUsers { get; set; }
-		public List<ApplicationUser> ManagerUsers { get; set; }
+		public ApplicationUser User { get; set; }
+		[Required, Display(Name = "Имя пользователя")]
+		public string UserName { get; set; }
+		public string Id { get; set; }
+
+		//[Required]
+		[Display(Name = "Email"), EmailAddress]
+		public string Email { get; set; }
+
+		//[Required]
+		[Display(Name = "Номер телефона"), Phone]
+		public string PhoneNumber { get; set; }
+		[Required, DataType(dataType: DataType.Password), Display(Name = "Пароль")]
+		public string Password { get; set; }
+
+		[Required, Compare(otherProperty: "Password", ErrorMessage = "Пароли не совпадают!"), DataType(dataType: DataType.Password), Display(Name = "Подтвердить пароль")]
+		public string PasswordConfirm { get; set; }
 	}
 }
